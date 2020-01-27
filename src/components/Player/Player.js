6bdf907aa22181fd452fa3309e547dd8836ff640
Richard Allum <richard.allum@york.ac.uk>
2020-01-27 09:24:13 +0000

    Simplify Scorebox component's score changing

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -3,15 +3,11 @@ import React, {useState} from "react";
 
 const Scorebox = ({ score, setScore }) => {
 
-  const changeScore = newScore => {
-    setScore(newScore);
-  }
-
   return (
     <div className="card-content has-text-centered" style={{display: "flex", justifyContent: "space-around"}} >
-      <button className="button is-error" onClick={() => changeScore(score - 1)}>-1</button>
+      <button className="button is-error" onClick={() => setScore(score - 1)}>-1</button>
       <span className="title">{score || "0"}</span>
-      <button className="button is-success" onClick={() => changeScore(score + 1)}>+1</button>
+      <button className="button is-success" onClick={() => setScore(score + 1)}>+1</button>
     </div>
   );
 };
